f66ac2fe54f831ea50bd4343bce15fb34f556466
Paco Aranda <francis@argilla.io>
2024-02-12 22:33:34 +0100

    Define new question type: spans

--- src/argilla_server/enums.py
+++ src/argilla_server/enums.py
@@ -61,6 +61,7 @@ class QuestionType(str, Enum):
     label_selection = "label_selection"
     multi_label_selection = "multi_label_selection"
     ranking = "ranking"
+    spans = "spans"
 
 
 class MetadataPropertyType(str, Enum):
