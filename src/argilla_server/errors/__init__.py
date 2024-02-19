dd466c28909c5c7a4919e2fa1f684b7acb66e366
Paco Aranda <francis@argilla.io>
2024-02-19 09:57:07 +0100

    review and improve imports

--- src/argilla_server/errors/__init__.py
+++ src/argilla_server/errors/__init__.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .api_errors import *
+from .error_handler import *
 from .base_errors import *
