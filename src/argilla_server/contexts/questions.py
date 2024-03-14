135261ffe6969aa5b29bd97c520c2623cf431712
José Francisco Calvo <jose@argilla.io>
2024-03-14 13:17:54 +0100

    refactor: use SuggestionCreateValidator for suggestions upsert and remove questions models file

--- src/argilla_server/contexts/questions.py
+++ src/argilla_server/contexts/questions.py
@@ -21,17 +21,13 @@ from sqlalchemy.orm import selectinload
 
 import argilla_server.errors.future as errors
 from argilla_server.enums import QuestionType
-from argilla_server.models import (
-    Dataset,
-    LabelSelectionQuestionSettings,
-    Question,
-    QuestionSettings,
-    User,
-)
+from argilla_server.models import Dataset, Question, User
 from argilla_server.policies import QuestionPolicyV1, authorize
 from argilla_server.schemas.v1.questions import (
+    LabelSelectionQuestionSettings,
     LabelSelectionSettingsUpdate,
     QuestionCreate,
+    QuestionSettings,
     QuestionSettingsUpdate,
     QuestionUpdate,
     SpanQuestionSettingsCreate,
