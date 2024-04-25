3a1442c850a76151f4269e9583f8ec58fbd99f6e
Francisco Aranda <francis@argilla.io>
2024-04-25 15:36:42 +0200

    chore: Remove commented code

--- src/argilla_server/schemas/v1/records_bulk.py
+++ src/argilla_server/schemas/v1/records_bulk.py
@@ -21,9 +21,6 @@ from argilla_server.schemas.v1.records import Record, RecordCreate, RecordUpsert
 RECORDS_BULK_CREATE_MIN_ITEMS = 1
 RECORDS_BULK_CREATE_MAX_ITEMS = 500
 
-RECORDS_BULK_UPDATE_MIN_ITEMS = 1
-RECORDS_BULK_UPDATE_MAX_ITEMS = 500
-
 RECORDS_BULK_UPSERT_MIN_ITEMS = 1
 RECORDS_BULK_UPSERT_MAX_ITEMS = 500
 
@@ -50,29 +47,6 @@ class RecordsBulkCreate(BaseModel):
             raise ValueError("External IDs must be unique")
 
         return items
-
-
-# class RecordUpdate(_RecordUpdate):
-#     id: Optional[UUID]
-#     external_id: Optional[str]
-#
-#     @root_validator(skip_on_failure=True)
-#     @classmethod
-#     def check_id_or_external_id(cls, values: dict) -> dict:
-#         """Check that either 'id' or 'external_id' is provided"""
-#         record_id = values.get("id")
-#         external_id = values.get("external_id")
-#
-#         if record_id is None and external_id is None:
-#             raise ValueError("Either 'id' or 'external_id' must be provided")
-#
-#         return values
-#
-#
-# class RecordsBulkUpdate(RecordsBulkCreate):
-#     items: List[RecordUpdate] = Field(
-#         ..., min_items=RECORDS_BULK_UPDATE_MIN_ITEMS, max_items=RECORDS_BULK_UPDATE_MAX_ITEMS
-#     )
 
 
 class RecordsBulkUpsert(RecordsBulkCreate):
